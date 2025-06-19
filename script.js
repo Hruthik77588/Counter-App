5905006c87f2e3b2a5d936e7739902e4a281920a
Hruthik77588 <hruthikbarmukh@gmail.com>
2025-06-19 12:26:07 +0530

    project completed

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-const Count=document.querySelector(".count")
+const Count = document.querySelector(".count")
 
-const Decreament=document.querySelector(".decrease")
-const Increament=document.querySelector(".increase")
-const  ResetBtn=document.querySelector('.resetbtn')
-const changeBy=document.querySelector(".changeBy")
-Decreament.addEventListener('click',()=>{
-   const countValue=parseInt(Count.innerText);
-    console.log(countValue)
-   Count.innerText=countValue -1
-            
-})
-Increament.addEventListener('click',()=>{
-         const countValue=parseInt(Count.innerText);
-   console.log(countValue)
-   Count.innerText=countValue +1
-           
-})
+const Decreament = document.querySelector(".decrease")
+const Increament = document.querySelector(".increase")
+const changeBy = document.querySelector(".changeBy")
 
-changeBy.addEventListener("input",()=>{
-    const changeByValue=parseInt(changeBy.value)
-    console.log(inputValue)
-   Count.value*inputValue
+const ResetBtn = document.querySelector('.resetbtn')
+
+Decreament.addEventListener('click', () => {
+   const countValue = parseInt(Count.innerText);
+   const changeByValue = parseInt(changeBy.value)
+   Count.innerText = countValue - changeByValue
+
+})
+Increament.addEventListener('click', () => {
+   const countValue = parseInt(Count.innerText);
+   const changeByValue = parseInt(changeBy.value)
+   Count.innerText = countValue + changeByValue
+
 })
 
-ResetBtn.addEventListener('click',()=>{
-    Count.innerText=0
+ResetBtn.addEventListener('click', () => {
+   Count.innerText = 0
+   changeBy.value=0
 })
